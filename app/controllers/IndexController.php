9790127bdd72b435b8cccc5262dfd973ba0bd205
Muhammad Hannan Ali <mdhannan.info@gmail.com>
2020-09-20 13:50:12 +0600

    Load default controller and method

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php 
+
+class IndexController extends DController
+{
+
+    public function __construct()
+    {
+       // parent::__construct();
+    }
+    public function home(){
+        echo "Home Content form IndexController";
+    }
+}
--- index.php
+++ index.php
@@ -3,16 +3,33 @@
 include './system/libs/Main.php';
 include './system/libs/DController.php';
 
-$url = $_GET['url'];
-$url = rtrim($url,'/');
-$url = explode("/", $url);
-
+$url = isset($_GET['url']) ? $_GET['url'] : NULL;
+if($url != NULL){
+    $url = rtrim($url, '/');
+    $url = explode("/", filter_var($url, FILTER_SANITIZE_URL));
+}else {
+    unset($url);
+}
 //Loading Controller dynamically
-include './app/controllers/'.$url[0].'.php';
+if(isset($url[0])){
+    include './app/controllers/'.$url[0].'.php';
+    $ctlr = new $url[0]();
+    if(isset($url[2])){
+        $ctlr->$url[1]($url[2]);
+    }
+    else{
+        if(isset($url[1])){
+            $ctlr->$url[1]();    
+        }
+        else{  
+        }
+    }
+}else{  
+    include './app/controllers/IndexController.php';
+    $ctlr = new IndexController();
+    $ctlr->home();
+}
 
-$ctlr = new $url[0]();
-$string = str_replace('"', "", $url[1]);
-$ctlr->$string($url[2]);
 
 
 
